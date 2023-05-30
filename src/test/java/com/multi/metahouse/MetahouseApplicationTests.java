c7e7cec4c1067a3b115b9c9a4dcc11c814eddda0
DojinP <coah2013@naver.com>
2023-05-30 17:12:17 +0900

    Initial Setting Commit

---
+++ src/test/java/com/multi/metahouse/MetahouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.metahouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetahouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
