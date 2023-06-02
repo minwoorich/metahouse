ab7e7ebe96cdd4d7a4dd88e491fb043b122c0ae4
DojinP <coah2013@naver.com>
2023-06-02 17:48:44 +0900

    pdj/dashboard_update

--- src/main/java/com/multi/metahouse/controller/DashboardController.java
+++ src/main/java/com/multi/metahouse/controller/DashboardController.java
@@ -3,22 +3,28 @@ package com.multi.metahouse.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
-	@GetMapping("/")
-	public String index() {
-		return "Thymeleaf/index";
+	@GetMapping("/index")
+	public String viewIndex(Model model) {
+		return "dashboard/content/index";
 	}
 	
-	@GetMapping("/dashboard")
-	public String viewDashboard(Model model, String tab) {
-		model.addAttribute("tab", tab);
-		return "Thymeleaf/main_layout";
+	@GetMapping("/management")
+	public String viewManagement(Model model) {
+		return "dashboard/content/member_management";
+	}
+	
+	@GetMapping("/report")
+	public String viewReport(Model model) {
+		return "dashboard/content/member_report";
+	}
+	
+	@GetMapping("/statistics")
+	public String viewStatistics(Model model) {
+		return "dashboard/content/dash_statistics";
 	}
 }
-
-
-
-
-
