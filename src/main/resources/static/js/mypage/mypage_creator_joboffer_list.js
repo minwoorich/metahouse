906f5b86a033b5f7e66f421499e5b342d29b5ec4
Minwoo Jhung <minwoorich@naver.com>
2023-06-02 20:46:08 +0900

    첫번째 커밋
    페이지
    - mypage의 프로젝트 등록, 구인글 등록, 구인글 목록
    
    미완성 된 것들
    - 모든 페이지에 필요한 모달
    - 모든 페이지에 필요한 JS
    - 구인글 등록 페이지에서 미리보기 창
    - 2차 카테고리 추가
    - 구인글 전용 progress-bar 수정

---
+++ src/main/resources/static/js/mypage/mypage_creator_joboffer_list.js
@@ -0,0 +1,47 @@
+///////////////mypage_creator_joboffer_list (구인관리) 시작/////////////////////////
+
+//신청현황 드롭다운
+$(document).ready(function () {
+    $(".my-dropdown button").click(function () {
+        $(this).siblings(".my-dropdown-contents").toggle();
+    });
+
+
+    //옵션 드롭다운
+    $(".job-offer-option").click(function () {
+        $(this).find(".job-offer-option-dropdown").toggle();
+    });
+
+
+    //옵션 드롭다운 - 삭제하기
+    $(".job-offer-option-dropdown li a").click(function () {
+        $(this)
+    });
+
+    //카드 호버
+    $(".job-offer-card").mouseover(function (event) {
+        event.stopPropagation();
+        var target = $(event.target); // 이벤트가 발생한 요소
+        $(target).css({
+            "background-color": "rgba(0,0,0,0.1)",
+            "transition": "0.5s"
+        });
+
+    }).mouseout(function (event) {
+        event.stopPropagation();
+        var target = $(event.target); // 이벤트가 발생한 요소
+        $(target).css("background-color", ""); // 원래 상태로 돌아가는 예시 (배경색 제거)
+
+    });
+
+
+    //카드 클릭 -> 상세보기 페이지로 이동
+    $(".job-offer-card").on("click", function (event) {
+        const target = event.target;
+        // 구인 글 고유번호 추출 + 컨트롤러 전송
+        //...
+        //window.location.href = 
+    });
+});
+
+////////////////////////mypage_creator_joboffer_list (구인관리) 끝///////////////////////////
