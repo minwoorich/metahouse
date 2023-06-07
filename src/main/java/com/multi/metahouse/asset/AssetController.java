f9020f6844167af8cb8282f71691a979ea089dd3
sminxxi <osm0507@naver.com>
2023-06-08 00:05:26 +0900

    asset 판매 등록, 상세보기 페이지 추가

--- src/main/java/com/multi/metahouse/asset/AssetController.java
+++ src/main/java/com/multi/metahouse/asset/AssetController.java
@@ -1,6 +1,7 @@
 package com.multi.metahouse.asset;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class AssetController {
 	@RequestMapping("/saleslist")
 	public String assetSale() {
 		return "market_sm/asset/asset_saleslist";
+	}
+	
+	@GetMapping("/asset-form-01")
+	public String assetForm01() {
+		return "market_sm/asset/mypage_asset_assetform01";
+	}
+	
+	@GetMapping("/asset-form-02")
+	public String assetForm02() {
+		return "market_sm/asset/mypage_asset_assetform02";
 	}
 }
