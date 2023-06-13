57c2827784b94f0b7e31302a999e628e52f0e830
ohseungeon <ohseungeon@192.168.1.96>
2023-06-13 13:43:53 +0900

    js추가

--- src/main/resources/static/js/market/purchase.js
+++ src/main/resources/static/js/market/purchase.js
@@ -1,4 +1,39 @@
 $(document).ready(function(){
+	//up-down
+	$("input:checkbox").siblings().children("button").hide();
+
+	$("input:checkbox").on('click', function() {
+		if($(this).prop('checked')){
+			$(this).siblings().children("button").show();
+			$(this).siblings().children("input").val("1");
+		} else {
+			$(this).siblings().children("button").hide();
+			$(this).siblings().children("input").val("0");
+		}
+	});
+	
+	$(".up").on("click", function(){
+		const $target = $(event.target); //이벤트 발생한 요소
+		const display = $target.siblings(".updown_display");
+		const value = display.val();
+		if(value==10){
+			alert("추가옵션 수량이 과다할 경우 의뢰가 취소될 수 있습니다.")
+			$target.siblings(".updown_display").val(parseInt(value) + 1);
+		}else{ 
+			$target.siblings(".updown_display").val(parseInt(value) + 1);
+		}
+	});
+	$(".down").on("click", function(){
+		const $target = $(event.target); //이벤트 발생한 요소
+		const display = $target.siblings(".updown_display");
+		const value = display.val();
+		if(value==1){
+			alert("최소 1개 이상 선택하여야 합니다.")
+		}else{ 
+			$target.siblings(".updown_display").val(parseInt(value) - 1); //해당값을 적용해준다.
+		}
+	});
+	
 	//사용자가 입력한 글자수 카운팅
     $("textarea").on("input", function (event) {
         const $target = $(event.target); //이벤트 발생한 요소
