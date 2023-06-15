d65fb889948f390daf3b44baf8acfed8c82f63e6
Minwoo Jhung <minwoorich@naver.com>
2023-06-15 22:20:08 +0900

    충돌 수정
    AssetDTO
    충돌 수정
    
    ProjectController
    ProjectControllerMinwoo라는 파일을 따로 제꺼 전용으로 생성했습니다
    
    application.properties
    이거 음 왜 충돌 나는지 모르겠지만 수정해봤는데 뭔가 또 충돌 날것같은..?

--- src/main/java/com/multi/metahouse/project/controller/ProjectController.java
+++ src/main/java/com/multi/metahouse/project/controller/ProjectController.java
@@ -1,60 +1,29 @@
 package com.multi.metahouse.project.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.multi.metahouse.domain.dto.project.ProjectDTO;
-import com.multi.metahouse.project.service.ProjectFileService;
-import com.multi.metahouse.project.service.ProjectOptionService;
-import com.multi.metahouse.project.service.ProjectService;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
-	ProjectService projectService;
-	ProjectFileService projectFileService;
-	ProjectOptionService projectOptionService;
-	
-	
-	@Autowired
-	public ProjectController(ProjectService projectService, ProjectFileService projectFileService,ProjectOptionService projectOptionService) {
-		super();
-		this.projectService = projectService;
-		this.projectFileService = projectFileService;
-		this.projectOptionService = projectOptionService;
-	}
-
-
 	@GetMapping("/project-product")
 	public String projectFormlist() {
-		return "market_sm/project/mypage_project_product_list";
+		return "project/project_product_list";
 	}
 	
-	
-	/////////////////민우 영역/////////////////////
 	@GetMapping("/project-form-01")
-	public String insertProjectForm01(Model model) {
-		ProjectDTO projectDto = new ProjectDTO();
-		model.addAttribute("projectDto", projectDto);
-		return "redirect:project/projectform01";
+	public String projectForm01() {
+		return "mypage/mypage_creator_projectform01";
 	}
-	@PostMapping("/project-form-01")
-	public String projectForm01Page() {
-		return "project/projectform02";
-	}
-	
-	
 	
 	@GetMapping("/project-form-02")
 	public String projectForm02() {
-		return "project/projectform02";
+		return "mypage/mypage_creator_projectform02";
 	}
+	
 	@GetMapping("/project-form-03")
 	public String projectForm03() {
-		return "project/projectform03";
+		return "mypage/mypage_creator_projectform03";
 	}
 }
