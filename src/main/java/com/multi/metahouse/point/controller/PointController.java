f3e6cd623958986ba261ad9c4dad1e3acd92a388
DojinP <coah2013@naver.com>
2023-06-20 17:58:10 +0900

    pdj/point-paging 1. 테스트코드 삭제 2. 페이징 기능 구현 3. 문제점  3-1) 페이징 넘버링을 타임리프가 아닌 방법으로 구현      > 타임리프는 서버사이드 렌더링이므로 ajax로 업데이트할 수 없음

--- src/main/java/com/multi/metahouse/point/controller/PointController.java
+++ src/main/java/com/multi/metahouse/point/controller/PointController.java
@@ -1,14 +1,20 @@
 package com.multi.metahouse.point.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.multi.metahouse.domain.dto.point.MyPointDTO;
+import com.multi.metahouse.domain.entity.point.ChargedPointInfo;
+import com.multi.metahouse.domain.entity.point.ConsumedPointInfo;
 import com.multi.metahouse.domain.entity.user.User;
 import com.multi.metahouse.point.service.PointService;
 
@@ -30,14 +36,35 @@ public class PointController {
 		
 		MyPointDTO myPoint = service.getMyPointDTO((User)session.getAttribute("loginUser"));
 		
-		System.out.println("myPoint : " + myPoint);
+//		System.out.println("myPoint : " + myPoint);
 		
 		mav.addObject("cgpilist", myPoint.getChargedInfoList());
 		mav.addObject("cspilist", myPoint.getConsumedInfoList());
 		mav.addObject("tcgp", myPoint.getTotalChargedPoint());
 		mav.addObject("tcsp", myPoint.getTotalConsumedPoint());
+		mav.addObject("tpcgpi", myPoint.getTotalPageOfChargedPointInfo());
+		mav.addObject("tpcspi", myPoint.getTotalPageOfConsumedPointInfo());
+		
 		mav.setViewName("point/point");
 		return mav;
+	}
+	
+	/* ajax 페이징 (chargedPointInfoList) */
+	@PostMapping(value = "/page/cgpointinfo", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public List<ChargedPointInfo> cgPageList(HttpSession session, String pageNum, String location) {
+		List<ChargedPointInfo> list = service.chargePointInfoList((User)session.getAttribute("loginUser"), Integer.parseInt(pageNum)-1);
+		
+		return list;
+	}
+	
+	/* ajax 페이징 (consumedPointInfoList) */
+	@PostMapping(value = "/page/cspointinfo", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public List<ConsumedPointInfo> csPageList(HttpSession session, String pageNum, String location) {
+		List<ConsumedPointInfo> list = service.consumePointInfoList((User)session.getAttribute("loginUser"), Integer.parseInt(pageNum)-1);
+		
+		return list;
 	}
 	
 	@GetMapping("/charge")
@@ -49,8 +76,8 @@ public class PointController {
 	public String pointCharge(HttpSession session, int point) {
 		User loginUser = (User)session.getAttribute("loginUser");
 		
-		System.out.println("point = " + point);
-		System.out.println("loginUser = " + loginUser);
+//		System.out.println("point = " + point);
+//		System.out.println("loginUser = " + loginUser);
 		
 		service.chargePoint(loginUser, point);
 		
--- src/main/java/com/multi/metahouse/point/repository/dao/PointDAO.java
+++ src/main/java/com/multi/metahouse/point/repository/dao/PointDAO.java
@@ -2,6 +2,8 @@ package com.multi.metahouse.point.repository.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
+
 import com.multi.metahouse.domain.dto.point.MyPointDTO;
 import com.multi.metahouse.domain.entity.point.ChargedPointInfo;
 import com.multi.metahouse.domain.entity.point.ConsumedPointInfo;
@@ -15,9 +17,18 @@ public interface PointDAO {
 	// 포인트 사용하기
 	public void consumePoint(User loginUser, int consumeAmount);
 	// 포인트 충전내역 조회하기
-	public List<ChargedPointInfo> chargePointInfoList(String userId);
+	public List<ChargedPointInfo> chargePointInfoList(User loginUser);
 	// 포인트 사용내역 조회하기
-	public List<ConsumedPointInfo> consumePointInfoList(String userId);
+	public List<ConsumedPointInfo> consumePointInfoList(User loginUser);
 	// MyPointDTO 조회하기
 	public MyPointDTO getMyPointDTO(User loginUser);
+	
+	// pageRequest 얻어오기
+	public PageRequest getPageRequest(String id);
+	
+	// 포인트 충전내역 조회하기 (페이징)
+	public List<ChargedPointInfo> chargePointInfoList(User loginUser, int pageNo);
+	
+	// 포인트 사용내역 조회하기
+	public List<ConsumedPointInfo> consumePointInfoList(User loginUser, int pageNo);
 }
