b3e0366ffc911477cdb65d1156113f1a14f3c1c7
DojinP <coah2013@naver.com>
2023-06-24 15:55:59 +0900

    pdj/chat-fn 1. 디버깅을 위해 log4j dependancy 추가 2. log4j 관련 config 파일 작성 3. 채팅뷰 load 기능 구현

--- src/main/java/com/multi/metahouse/chat/repository/dao/ChatDAOImpl.java
+++ src/main/java/com/multi/metahouse/chat/repository/dao/ChatDAOImpl.java
@@ -1,10 +1,15 @@
 package com.multi.metahouse.chat.repository.dao;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.multi.metahouse.domain.dto.ChatroomDTO;
+import com.multi.metahouse.domain.dto.chat.ChatroomDTO;
+
 @Repository
 public class ChatDAOImpl implements ChatDAO {
 
@@ -26,14 +31,33 @@ public class ChatDAOImpl implements ChatDAO {
     }
 
     @Override
-    public void deleteChatroom(int chatroomId) {
-        sqlSession.delete("deleteChatroom", chatroomId);
+    public void deleteChatroom(String targetId) {
+        sqlSession.delete("deleteChatroom", targetId);
     }
 
     @Override
-    public ChatroomDTO getChatroomById(int chatroomId) {
-        return sqlSession.selectOne("getChatroomById", chatroomId);
+    public List<ChatroomDTO> getChatroomById(String userId) {
+		System.out.println("dao 메소드 실행!");
+    	List<ChatroomDTO> chatrooms = sqlSession.selectList("getChatroomById", userId);
+        return chatrooms;
     }
+    
+//    @Override
+//    public String getLastMsgDateById(int chatroom_id) {
+//    	System.out.println("dao 메소드 실행!! chatroom_id = " + chatroom_id);
+//    	Date last_msg_date = sqlSession.selectOne("getLastMsgDateById", chatroom_id);
+//    	System.out.println("쿼리결과:" + last_msg_date);
+//    	String last_msg_date_str = "";
+//    	/* Date 포맷 가공 */
+//    	// Date가 null 이 아닌경우에만 포맷
+//    	if(last_msg_date != null) {
+//    		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//        	last_msg_date_str = format.format(last_msg_date);
+//        	System.out.println("가공된 Date:" + last_msg_date_str);
+//    	}
+//    	
+//        return last_msg_date_str;
+//    }
 
     // 추가적인 메서드들을 필요에 따라 구현합니다.
 }
--- src/main/java/com/multi/metahouse/chat/service/ChatService.java
+++ src/main/java/com/multi/metahouse/chat/service/ChatService.java
@@ -1,17 +1,21 @@
 package com.multi.metahouse.chat.service;
 
 import java.util.List;
+import java.util.Map;
 
-import com.multi.metahouse.domain.dto.ChatroomDTO;
+import com.multi.metahouse.domain.dto.chat.ChatMsgDTO;
+import com.multi.metahouse.domain.dto.chat.ChatroomDTO;
 
 public interface ChatService {
-
-	 void createChatRoom(ChatroomDTO chatRoomDTO);
-	    void updateChatroom(ChatroomDTO chatRoomDTO);
-	    void deleteChatroom(int chatroomId);
-	    ChatroomDTO getChatroomById(int chatroomId);
-	    List<ChatroomDTO> getAllChatRooms();
-	}
+ 	void createChatroom(ChatroomDTO chatroomDTO);
+    void updateChatroom(ChatroomDTO chatroomDTO);
+    void deleteChatroom(String targetId);
+    
+    //Map<String, Object> getChatroomView(String userId);
+    List<ChatroomDTO> getChatroomView(String userId);
+    
+    List<ChatMsgDTO> getChatMsgById(int chatroomId);
+}
 
 
 
