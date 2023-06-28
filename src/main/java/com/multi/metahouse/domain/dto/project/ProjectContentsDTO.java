3e18d574a12a53d651ca4087c582172751d49105
ohseungeon <ohseungeon@192.168.1.96>
2023-06-28 12:00:56 +0900

    상세이미지 보기 버그 해결

--- src/main/java/com/multi/metahouse/domain/dto/project/ProjectContentsDTO.java
+++ src/main/java/com/multi/metahouse/domain/dto/project/ProjectContentsDTO.java
@@ -20,7 +20,7 @@ public class ProjectContentsDTO {
 	private int projectFileNo;
 //	private String project_content_id;
 //	private String project_id;
-//	private String project_store_filename;
+	private String project_store_filename;
 //	private String project_file_no;
 	
 	public ProjectContentsEntity toEntity() {
