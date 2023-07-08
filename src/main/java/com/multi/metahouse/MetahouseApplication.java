374c6ef1809cdf51aeb4478f4a8e5e5dd216d30e
Minwoo Jhung <minwoorich@naver.com>
2023-07-08 19:14:45 +0900

    upload폴더 안에 파일 삭제

--- src/main/java/com/multi/metahouse/MetahouseApplication.java
+++ src/main/java/com/multi/metahouse/MetahouseApplication.java
@@ -2,9 +2,19 @@ package com.multi.metahouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MetahouseApplication {
+public class MetahouseApplication extends SpringBootServletInitializer {
+
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MetahouseApplication.class, args);
