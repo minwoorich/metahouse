cb439dea5f0429098ea7d3837d2e4b572625d143
DojinP <coah2013@naver.com>
2023-07-08 13:01:39 +0900

    pdj/add-fn 1. dashboard css (member_management)수정 2. deleteAccount mapping 추가

--- src/main/java/com/multi/metahouse/dashboard/controller/DashboardController.java
+++ src/main/java/com/multi/metahouse/dashboard/controller/DashboardController.java
@@ -25,99 +25,96 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @RequestMapping("/dashboard")
 public class DashboardController {
-	
+
 	DashboardService service;
-	
+
 	@Autowired
 	public DashboardController(DashboardService service) {
 		this.service = service;
 	}
-	
-	/* View Mapping */
+
+	/* Dashboard 메인 페이지 */
 	@GetMapping("/")
 	public String viewDashboardMain(Model model) {
 		return "dashboard/content/dashboard_statistics";
 	}
-	
+
+	/* 회원관리 페이지 */
 	@GetMapping("/member/management")
-	public String viewMemberManagement(Model model,
-			@RequestParam(defaultValue = "1") int page
-			) {
-		
-		
+	public String viewMemberManagement(Model model, @RequestParam(defaultValue = "1") int page) {
+
 		int size = 10;
 		PageRequest pageReq = PageRequest.of(page - 1, size, Sort.by("createDate").descending());
-		
+
 		Page<User> userPage = null;
-		
-		
+
 		userPage = service.findALL(pageReq);
 		System.out.println(userPage.getSize());
 		int totalPage = userPage.getTotalPages();
-		List<Integer> pageBtnList = makePagingSeq(page , 5 , totalPage);
+		List<Integer> pageBtnList = makePagingSeq(page, 5, totalPage);
 		System.out.println(pageBtnList);
 		model.addAttribute("userList", userPage);
-		model.addAttribute("pages",pageBtnList);
+		model.addAttribute("pages", pageBtnList);
 		model.addAttribute("currentPage", page);
-		model.addAttribute("totalPage",totalPage);
-		
-		
+		model.addAttribute("totalPage", totalPage);
+
 		return "dashboard/content/member_management";
 	}
-	
+
+	/* 회원 검색 페이지 (GET) */
 	@GetMapping("/member/search")
 	public String viewMemberSearch(Model model) {
 		return "dashboard/content/member_search";
 	}
-	
+
+	/* 회원 검색 페이지 (POST) */
 	@PostMapping("/member/search")
 	@ResponseBody
 	public List<User> search_user(String keyword) {
 		System.out.println(keyword);
 		List<User> userList = service.findByUserIdLike(keyword);
-		
+
 		return userList;
 	}
-	
+
+	/* 회원 수정 페이지 (GET) */
 	@GetMapping("/member/update")
 	public String viewMemberUpdate(Model model) {
 		return "dashboard/content/member_update";
 	}
+	
+	/* 회원 수정 페이지 (POST) */
 	@PostMapping("/member/update")
 	public String update_member(UserSearchResultDTO user) {
 		service.user_update(user);
 		return "redirect:/dashboard/member/search";
 	}
-	
-	
+
+	/* 신고 관리 페이지 */
 	@GetMapping("/member/report")
 	public String viewMemberReport(Model model) {
 		return "dashboard/content/member_report";
 	}
-	
+
+	/* 통계 조회 페이지 */
 	@GetMapping("/statistics")
 	public String viewStatistics(Model model) {
 		return "dashboard/content/dashboard_statistics";
 	}
-	
-	
-	
-	
-	
-////////////////////////////////////
-	
-   public static List<Integer> makePagingSeq(int n, int size , int max){
-	   
-	      List<Integer> seq = new ArrayList<Integer>();
-	      int pageIndex = (n-1)/size;
-	      int start = pageIndex*size;
-	      
-	      for(int i= start+1; i<= start+size; i++) {
-	         if(i> max) break;
-	         seq.add(i);
-	      }
-	      
-	      return seq;
-   }
-////////////////////////////////////
+
+	/* 페이지 생성 메소드 (서비스 단으로 이동 예정) */
+	public static List<Integer> makePagingSeq(int n, int size, int max) {
+
+		List<Integer> seq = new ArrayList<Integer>();
+		int pageIndex = (n - 1) / size;
+		int start = pageIndex * size;
+
+		for (int i = start + 1; i <= start + size; i++) {
+			if (i > max)
+				break;
+			seq.add(i);
+		}
+
+		return seq;
+	}
 }
