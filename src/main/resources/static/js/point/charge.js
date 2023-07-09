64f235c13e775a97508a0f154b9b32f9c60b561c
Dojin Park <coah2013@naver.com>
2023-07-09 18:21:53 +0900

    charge 페이지  - 포인트 금액 직접 입력 구현

--- src/main/resources/static/js/point/charge.js
+++ src/main/resources/static/js/point/charge.js
@@ -1,5 +1,19 @@
-$(doucument).ready(function() {
-    $("#input_point").on("change", function() {
-        if() {}
+$(document).ready(function() {
+    $("input:radio[name=point]").on("click", function() {
+        $(this).prop("checked", true);
+        if($(this).attr("id") == "input_point") {
+            console.log("id=" + $(this).attr("id"));
+            $("#selected_point_div").show();
+            $("#selected_point").focus();
+        }else{
+            console.log("id=" + $(this).attr("id"));
+            $("#selected_point_div").hide();
+            $("#selected_point").val("");
+        }
+    })
+
+    $("#selected_point").on("keyup", function() {
+        $("#input_point").attr("value", $(this).val());
+        console.log($("#input_point").val());
     })
 })
